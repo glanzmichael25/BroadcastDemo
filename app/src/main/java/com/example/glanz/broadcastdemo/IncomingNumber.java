dc031423d036387e171f1b110efc0cbcd7cd2e4f
glanzmichael25 <glanz.michael.25@gmail.com>
2018-05-30 22:28:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/glanz/broadcastdemo/IncomingNumber.java
@@ -0,0 +1,24 @@
+package com.example.glanz.broadcastdemo;
+
+public class IncomingNumber {
+
+    private int id;
+    private String number;
+
+    public  IncomingNumber(int id, String number){
+        this.setId(id);
+        this.setNumber(number);
+    }
+    public  int getId(){
+        return id;
+    }
+    public  void  setId(int id){
+        this.id = id;
+    }
+    public String getNumber(){
+        return  number;
+    }
+    public  void  setNumber(String number){
+        this.number = number;
+    }
+}
